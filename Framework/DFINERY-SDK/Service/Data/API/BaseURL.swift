15985eb67da466ea7523dc53926b19e402d1d5bc
JiHoonAHN <ahnjh1028@naver.com>
2022-08-06 18:28:34 +0900

    [CREATE] API Start

---
+++ Framework/DFINERY-SDK/Service/Data/API/BaseURL.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct BaseURL {
+    var dfineryBaseURL : URL{
+        return URL(string: "http://adbrix-sdk-assignment-backend-115895936.ap-northeast-1.elb.amazonaws.com")!
+    }
+}
+
---
+++ Framework/DFINERY-SDK/Service/Data/API/HTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPClient.swift
+//  DFINERY-SDK
+//
+//  Created by Ji-hoon Ahn on 2022/08/06.
+//
+
+import Foundation

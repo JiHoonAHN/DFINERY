b7527355edbfbe31a58acd027e5deaa1e57599e3
JiHoonAHN <ahnjh1028@naver.com>
2022-08-08 03:43:50 +0900

    [CREATE] Sample Project & Setting

---
+++ Sample/DFINERY-Sample/Sources/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DFINERY-Sample
+//
+//  Created by Ji-hoon Ahn on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+

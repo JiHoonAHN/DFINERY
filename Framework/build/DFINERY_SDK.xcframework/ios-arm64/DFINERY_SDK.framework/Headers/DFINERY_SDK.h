3900f06252828b002256591f8c10d5374d524c4f
JiHoonAHN <ahnjh1028@naver.com>
2022-08-12 04:28:19 +0900

    [CREATE] xcframework

---
+++ Framework/build/DFINERY_SDK.xcframework/ios-arm64/DFINERY_SDK.framework/Headers/DFINERY_SDK.h
@@ -0,0 +1,18 @@
+//
+//  DFINERY_SDK.h
+//  DFINERY-SDK
+//
+//  Created by Ji-hoon Ahn on 2022/08/06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DFINERY_SDK.
+FOUNDATION_EXPORT double DFINERY_SDKVersionNumber;
+
+//! Project version string for DFINERY_SDK.
+FOUNDATION_EXPORT const unsigned char DFINERY_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFINERY_SDK/PublicHeader.h>
+
+
